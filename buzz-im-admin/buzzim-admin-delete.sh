923d3362a4e1779c422a45cf5a68abca039def5d
John Situ <john.situ@dialogic.com>
2019-12-30 14:58:10 -0500

    buzz im admin update

--- buzz-im-admin/buzzim-admin-delete.sh
+++ buzz-im-admin/buzzim-admin-delete.sh
@@ -6,5 +6,8 @@ kubectl create -f buzzim-admin-delete-db-job.yaml
 kubectl -n buzzim-admin wait --for=condition=complete --timeout=30s job/buzzim-admin-delete-db
 
 # Delete the namespace for the tool, this will cleanup everything else.
-kubectl delete -f buzzim-admin-namespace.yaml
+kubectl delete -f buzzim-admin-namespace.yaml &
 
+sleep 20s 
+
+../delete-stuck-namespace.sh buzzim-admin
